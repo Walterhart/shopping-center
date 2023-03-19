64ce1b675fedfc2bea416dce461db303ad53a666
walterhart <walterhart1996@gmail.com>
2023-03-19 15:04:57 -0400

    Setup file structure

---
+++ shopping-center/src/data/items.ts
@@ -0,0 +1,19 @@
+export default{
+    "products": [
+        {
+            "id": "1",
+            "name": "Clock",
+            "price": 40.00
+        },
+        {
+            "id": "2",
+            "name": "Scorch Dress",
+            "price": 59.99
+        },
+        {
+            "id": "3",
+            "name": "Sony Earbuds",
+            "price": 29.99
+        }
+    ]
+}
