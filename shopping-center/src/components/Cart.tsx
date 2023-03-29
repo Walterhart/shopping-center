b31b7b0bdcdabb3f0012a2a3a89ac963f7262e6c
Walterhart <walterwhart1@gmail.com>
2023-03-29 13:01:56 -0600

    Add checkout feature

--- shopping-center/src/components/Cart.tsx
+++ shopping-center/src/components/Cart.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import useCart from '../hooks/useCart';
+import { useState } from 'react';
+import CartCheckout from './CartCheckout';
+
 
 function Cart() {
+  const [confirm, setConfirm] = useState<boolean>(false)
+  const {dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart} = useCart()
+  
+  const onSubmit =() => {
+    dispatch( { type: REDUCER_ACTIONS.SUBMIT})
+    setConfirm(true)
+  }
+
+  const content = confirm 
+  ? <h2> Your order has been completed! Have a great day </h2>
+  :
+  <>
+   <h2> className</h2>
+   <ul>
+    {cart.map(item => {
+      return <CartCheckout
+      key = {item.sku}
+      item = {item}
+      dispatch = {dispatch}
+      REDUCER_ACTIONS = { REDUCER_ACTIONS}
+      />
+    })}
+
+   </ul>
+   <div className="cart--items">
+     <p> Total Items: {totalItems}</p>
+     <p>Total Price: {totalPrice}</p>
+     <button className="cart--btn" disabled = {!totalItems} onClick = {onSubmit}> Place order </button>
+   </div>
+  </>
   return (
-    <div>Cart</div>
+    <main className="cart">
+      {content}
+    </main>
   )
 }
 
---
+++ shopping-center/src/components/CartCheckout.tsx
@@ -0,0 +1,69 @@
+import React, { ChangeEvent } from 'react'
+import { CartItemType, ReducerActionType, ReducerAction } from '../context/modules/CartProvider';
+import { ReactElement } from 'react';
+
+type Props ={
+    item: CartItemType,
+    dispatch: React.Dispatch<ReducerAction>,
+    REDUCER_ACTIONS: ReducerActionType
+}
+const CartCheckout = ({item, dispatch, REDUCER_ACTIONS}: Props) => {
+    const imgPath: string = new URL(`../assets/images/${item.sku}.jpeg`, import.meta.url).href
+
+    const subtotal : number = (item.quantity  * item.price)
+
+    // max quantity
+    const highestQuantity: number  = 20 > item.quantity ? 20 : item.quantity
+
+    // quantity options values
+    const optionValues: number[] = [...Array(highestQuantity).keys()]. 
+    map(i => i + 1)
+ 
+    // quanity options
+    const options: ReactElement[] = optionValues.map(val =>{
+        return <option key = {`opt${val}`} value = { val}>
+              {val}
+             </option>
+    })
+
+    // change quantity
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) =>{
+        dispatch({
+            type: REDUCER_ACTIONS.QUANTITY,
+            payload: { ...item, quantity: Number(e.target.value)}
+        })
+    }
+
+    // remove item
+    const onRemove =() => dispatch({
+        type: REDUCER_ACTIONS.REMOVE,
+        payload: item
+    })
+
+  
+    return (
+        <li className="cart--item">
+            <img src = {imgPath} alt ={item.name}/>
+            <div aria-label = "Item Name"> {item.name}</div>
+            <div aria-label = "Price per Item"> {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item.price)}</div>
+            <label >
+                Item Quantity
+            </label>
+            <select 
+            name="itemQuantity" 
+            id="itemQuantity" 
+            className="cart--select"
+            value ={item.quantity}
+            onChange={onChange}>
+                {options}
+            </select>
+            <div className="cart--subtotal">
+            {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(subtotal)}
+            </div>
+            <button className="cart--button"  title = "Remove Item From Cart" onClick ={onRemove}>
+                delete item
+            </button>
+        </li>)
+}
+
+export default CartCheckout
