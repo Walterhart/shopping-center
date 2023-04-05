dfaa5944f8c6e326a29edef7bc64c2aa27128c81
Walterhart <walterwhart1@gmail.com>
2023-04-05 02:21:52 -0400

    Create css to show number of item in car

--- shopping-center/src/components/Navbar.tsx
+++ shopping-center/src/components/Navbar.tsx
@@ -1,32 +1,30 @@
-import React from "react";
-
+import { BsFillCartFill } from "react-icons/bs";
 type Props = {
   viewCart: boolean;
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
 };
-
 // navbar
 function Navbar({ viewCart, setViewCart }: Props) {
   // button logic
   // view cart or items base on click
   const btn = viewCart ? (
     <button
-      className="btn rounded-pill btn-secondary"
+      className="btn rounded-circle btn-secondary p-2"
       onClick={() => setViewCart(false)}
     >
-      View Items
+      <BsFillCartFill />
     </button>
   ) : (
     <button
-      className="btn rounded-pill btn-secondary"
+      className="btn rounded-circle btn-secondary position-relative p-2"
       onClick={() => setViewCart(true)}
     >
-      {" "}
-      View Cart
+      <BsFillCartFill />
+      <div className="position-absolute top-100 start-100 translate-middle badge border border-light rounded-circle bg-danger p-2">
+        3
+      </div>
     </button>
   );
-
   return <nav className="nav">{btn}</nav>;
 }
-
 export default Navbar;
